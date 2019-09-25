55fa7ed19ad1c7eaafb54b57a9158a10e1a955b4
mateuszpmroz <mateuszpmroz@gmail.com>
2019-09-25 13:51:53 +0200

    [91] Change youtube links to correct one

--- src/react/components/pages/editorPage/MagnifierModalMessage.tsx
+++ src/react/components/pages/editorPage/MagnifierModalMessage.tsx
@@ -5,19 +5,19 @@ export function MagnifierModalMessage(): ReactElement<any> {
         <div>
         Application does not have it's own magnifier, but you could use
         native magnifier of your operating system. Below are videos which
-        shows how to native magnifiers on macOS, Ubuntu Linux and Windows 10.
+        shows how to native magnifiers on macOS, Ubuntu and Windows 10.
         <br />
         <div>
             <i className="fab fa-apple" /> macOS -
             &nbsp;<a href="https://youtu.be/bz2JrEwvFWs" target="_blank">click here</a>
         </div>
         <div>
-            <i className="fab fa-ubuntu" /> Ubuntu Linux -
-            &nbsp;<a href="https://youtu.be/bz2JrEwvFWs" target="_blank">click here</a>
+            <i className="fab fa-ubuntu" /> Ubuntu -
+            &nbsp;<a href="https://youtu.be/pFwblGYjzSs" target="_blank">click here</a>
         </div>
         <div>
             <i className="fab fa-windows" /> Windows 10 -
-            &nbsp;<a href="https://youtu.be/bz2JrEwvFWs" target="_blank">click here</a>
+            &nbsp;<a href="https://youtu.be/xNhF1NJ78NE" target="_blank">click here</a>
         </div>
     </div>
     );
