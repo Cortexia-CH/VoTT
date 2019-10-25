edb7749fbde5b198a588d90f1f93171a0ccde164
ebreton <emmanuel.breton@cortexia.ch>
2019-10-25 11:10:03 +0200

    Prepared release 0.4.2

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.2-7-g1b77384'
+_release = '0.4.1-11-g19892ad'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.4.2'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '1b7738489b31c1f24e0affb4cb3be61f136aba07'
+_build = '19892adcab28201a6b0aae49c7e081c3e2fadb91'
