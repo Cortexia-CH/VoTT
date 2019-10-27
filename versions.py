88f8bba0550e09fb6b20668297ec04f7116c4ca0
ebreton <email>
2019-10-27 18:00:53 +0100

    Add port

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.4.2-2-g263e4d5'
+_release = '0.4.2-3-gd6cd264'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.4.3-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '263e4d586f18612ab8c0b3e1cda09048a11a4917'
+_build = 'd6cd2648ae6d08216556831cd46550aa048bff13'
